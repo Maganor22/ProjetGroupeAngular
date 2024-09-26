3019c003a0307d3bdef0d9548a8aa3710e7c1c1a
MathisAdrar <mathisb.buisson@gmail.com>
2024-09-26 14:14:44 +0200

    ajout fonction d'ajout d'article, ajout de la date aux articles

--- src/app/component/articles/articles.component.ts
+++ src/app/component/articles/articles.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Firestore, collectionData, collection, addDoc, deleteDoc, doc, Timestamp } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
@@ -8,22 +8,44 @@ interface Article {
   AuteurId: number;
   Titre: string;
   Contenu: string;
-  date: Timestamp;
+  date: number;
 }
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DatePipe],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
 export class ArticlesComponent {
   private firestore: Firestore = inject(Firestore)
   article$: Observable<Article[]>;
-
+  
+  
   constructor() {
     const articleCollection = collection(this.firestore, 'articles');
     this.article$ = collectionData(articleCollection,  { idField: 'id' }) as Observable<Article[]>
   }
+  
+  newArticle = "";
+  newArticleTitle = "";
+  
+  addUser() {
+    let today: number = Date.now();
+    
+    if (this.newArticle.trim()) {
+      const articleCollection = collection(this.firestore, 'articles');
+      addDoc(articleCollection, { Titre: this.newArticleTitle, Contenu: this.newArticle, date: today});
+      this.newArticleTitle = '';
+      this.newArticle = '';
+    }
+  }
+
+  // deleteUser(userId: string | undefined) {
+  //   if (userId) {
+  //     const taskDocRef = doc(this.firestore, 'utilisateurs', userId);
+  //     deleteDoc(taskDocRef);
+  //   }
+  // }
 }
