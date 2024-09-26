1c071f9ce3ea5fc5f35fa7b753077e4e75fc18c9
Ken <ken@MBP-de-Ken.lan>
2024-09-26 18:55:51 +0200

    home commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { ConnexionComponent } from './component/connexion/connexion.component';
 import { ArticlesComponent } from './component/articles/articles.component';
 import { MessagerieComponent } from './component/messagerie/messagerie.component';
+import { FriendlistComponent } from './component/friendlist/friendlist.component';
+
 // import { _ _ _Component } from './component/_ _ _/_ _ _.component';
 
 export const routes: Routes = [
     {path: '', component: ConnexionComponent},
     {path: 'articles', component: ArticlesComponent},
     {path: 'messagerie', component: MessagerieComponent},
+    {path: 'amis', component: FriendlistComponent},
 ];
---
+++ src/app/services/friend.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collectionData, collection, addDoc, deleteDoc, doc } from '@angular/fire/firestore';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthServiceService } from './auth-service.service';
+
+interface Friends {
+  id?: number;
+  isFriend: boolean;
+}
+interface fRequest {
+  id?: number;
+  name: string;
+}
+
+interface User {
+  id?: string;
+  name: string;  
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class FriendService {
+
+  private authService: AuthServiceService = inject(AuthServiceService);
+  private friending: FriendService = inject(FriendService);
+
+  users$: Observable<User[]>;
+  friends$: Observable<Friends[]>;
+  // userFriendRequests$: Observable<fRequest[]>;
+
+
+  private firestore: Firestore = inject(Firestore);
+
+  constructor() { 
+    const friendList = collection(this.firestore, 'friendlist');
+    const friendRequests = collection(this.firestore, 'friendlist/friend-request');
+    const userCollection = collection(this.firestore, 'utilisateurs');
+    this.friends$ = collectionData(friendList, { idField: 'id' }) as Observable<Friends[]>;
+    this.users$ = collectionData(userCollection, { idField: 'id' }) as Observable<User[]>;
+  }
+
+  addFriend(user: User) {
+    const friendRequests = collection(this.firestore, 'friendlist/friend-request');
+    addDoc(friendRequests, {name : user.name})
+  }
+  deleteFriend(userId: string | undefined) {
+    if (userId) {
+      const userDocRef = doc(this.firestore, 'friendlist', userId);
+      deleteDoc(userDocRef);
+    }
+  }
+}
