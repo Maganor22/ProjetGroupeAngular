2a2b64fc22344d36e9a3e1399a8a4c7e0122af7a
GitpushKen <kendivers@outlook.fr>
2024-09-27 12:25:24 +0200

    friendlist maj

--- src/app/component/friendlist/friendlist.component.ts
+++ src/app/component/friendlist/friendlist.component.ts
@@ -7,6 +7,7 @@ import { ConnexionComponent } from '../connexion/connexion.component';
 // import { FriendService } from '../../services/friend.service';
 import { AuthServiceService } from '../../services/auth-service.service';
 import { ListUserComponent } from "../list-user/list-user.component";
+import { updateDoc } from 'firebase/firestore';
 
 interface Friends {
   id?: string,
@@ -24,6 +25,7 @@ interface FriendRequests {
 interface User {
   id?: string;
   name: string;  
+  friend: string;
 }
 
 
@@ -39,19 +41,19 @@ interface User {
     <br>
 
 
-    <ul *ngIf="myName !== ''">
+    <ul *ngIf="myName !== '' ">
     <h2>Liste d'amis</h2>
-      <li *ngFor="let friend of friends$ | async">
+      <li *ngFor="let friend of myFriendList">
         {{ friend.name }}
         <button (click)="deleteFriend(friend.id)">Supprimer</button>
       </li>
       <br>
-      <h2>Invitation</h2>
+      <!-- <h2>Invitation</h2>
       <li *ngFor="let request of friendRequests$ | async">
         {{ request.name }}
         <button (click)="decline(request.id)">Refuser</button>
         <button (click)="accept(request.id)">Accepter</button>
-      </li>
+      </li> -->
     </ul>
   `,
   styleUrl: './friendlist.component.css'
@@ -66,10 +68,11 @@ export class FriendlistComponent {
 
   users$: Observable<User[]>;
   friends$: Observable<Friends[]>;
-  friendRequests$: Observable<FriendRequests[]>;
+  // friendRequests$: Observable<FriendRequests[]>;
   listUsers: User[] = [];
   friendList: any[] = [];
-  friendRequests: any[] = [];
+  myFriendList: User[] = [];
+  // friendRequests: any[] = [];
   private firestore: Firestore = inject(Firestore);
 
   constructor() { 
@@ -78,18 +81,60 @@ export class FriendlistComponent {
       this.myName = currentUser;
     }
 
-    // this.getUsers();
+    // this.getFriends();
     // this.getFriendRequests();
-    // this.getFriendsList();
+    this.getFriendsList();
 
     const friendRequests = collection(this.firestore, 'friend-requests')
     const friendList = collection(this.firestore, 'friendlist');
     this.friends$ = collectionData(friendList, { idField: 'id' }) as Observable<Friends[]>;
-    this.friendRequests$ = collectionData(friendRequests, { idField: 'id' }) as Observable<FriendRequests[]>;
+    // this.friendRequests$ = collectionData(friendRequests, { idField: 'id' }) as Observable<FriendRequests[]>;
     // this.friends$ = collectionData(friendRequests, { idField: 'id' }) as Observable<Friends[]>;
     const userCollection = collection(this.firestore, 'utilisateurs');
     this.users$ = collectionData(userCollection, { idField: 'id' }) as Observable<User[]>;
 
+  }
+
+  // getFriends() {
+  //   this.friends$.subscribe((friends) => {
+  //     this.friendList = friends;
+  //   });
+  // }
+
+  getFriendsList() {
+    const friendList = collection(this.firestore, 'friendlist');
+    const friends = collectionData(friendList, {
+      idField: 'id',
+    }) as Observable<any[]>;
+    friends.subscribe((friends) => {
+      this.friendList = friends;
+      this.myFriendList = this.friendList.filter(
+        (friend) => friend.friend == this.myName 
+      );
+    });
+    
+    // messages$.subscribe((messages) => {
+    //   // Trier les messages par date décroissante
+    //   this.listMessages = messages.sort((a, b) => b.date - a.date);
+      
+    //   // Filtrer les messages qui me concernent
+    //   this.listMyMessages = this.listMessages.filter(
+    //     (msg) => msg.receiverName === this.myName || msg.name === this.myName
+    //   );
+      
+    //   // Regrouper et trier les messages par utilisateur
+    //   this.groupMessagesByUser();
+    // });
+
+
+    return friends;
+  }
+
+    deleteFriend(userId: string | undefined) {
+    if (userId) {
+      const userDocRef = doc(this.firestore, 'friendlist', userId);
+      updateDoc(userDocRef, {friend: ''});
+    }
   }
 
   // addFriend(id:number) {
@@ -103,45 +148,33 @@ export class FriendlistComponent {
   //   });
   // }
 
-  getFriendRequests() {
-    const friendRequests = collection(this.firestore, 'friend-requests');
-    const request = collectionData(friendRequests, {
-      idField: 'id',
-    }) as Observable<any[]>;
-    request.subscribe((request) => {
-      this.friendRequests = request;
-    });
-    return request;
-  }
-
-  // getFriendsList() {
-  //   const friendList = collection(this.firestore, 'friendlist');
-  //   const friends = collectionData(friendList, {
+  // getFriendRequests() {
+  //   const friendRequests = collection(this.firestore, 'friend-requests');
+  //   const request = collectionData(friendRequests, {
   //     idField: 'id',
   //   }) as Observable<any[]>;
-  //   friends.subscribe((friends) => {
-  //     this.friendList = friends;
+  //   request.subscribe((request) => {
+  //     this.friendRequests = request;
   //   });
-  //   return friends;
+  //   return request;
   // }
 
-  deleteFriend(userId: string | undefined) {
-    if (userId) {
-      const userDocRef = doc(this.firestore, 'friendlist', userId);
-      deleteDoc(userDocRef);
-    }
-  }
-  decline(userId: string | undefined) {
-    if (userId) {
-      const userDocRef = doc(this.firestore, 'friend-requests', userId);
-      deleteDoc(userDocRef);
-    }
-  }
-  accept(userId: string | undefined) {
-    if (userId) {
-      const userDocRef = doc(this.firestore, 'friend-requests', userId);
-      deleteDoc(userDocRef);
-    }
-  }
+ 
+
+
+
+  // decline(userId: string | undefined) {
+  //   if (userId) {
+  //     const userDocRef = doc(this.firestore, 'friend-requests', userId);
+  //     deleteDoc(userDocRef);
+  //   }
+  // }
+
+  // accept(userId: string | undefined) {
+  //   if (userId) {
+  //     const userDocRef = collection(this.firestore, 'friend-requests', userId);
+  //     addDoc(userDocRef, {});
+  //   }
+  // }
 
 }
--- src/app/component/list-user/list-user.component.ts
+++ src/app/component/list-user/list-user.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { Firestore, collectionData, collection, addDoc, deleteDoc, doc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { AuthServiceService } from '../../services/auth-service.service';
 // import { FriendService } from '../../services/friend.service';
 
 interface User {
@@ -10,34 +11,79 @@ interface User {
   name: string;  
 }
 
+interface Friend {
+  id?: string,
+  isFriend: boolean,
+  name: string;  
+
+}
+
 
 @Component({
   selector: 'app-list-user',
   standalone: true,
   imports: [CommonModule],
-  template: `
-  <h2>Liste d'utilisateur</h2>
-  <ul>
-  <li *ngFor="let user of users$ | async">
-    {{ user.name }}
-    <button (click)="addFriend(user.id)">Suivre</button>
-
-  </li>
-</ul>`,
+  templateUrl: './list-user.component.html',
   styleUrl: './list-user.component.css'
 })
 export class ListUserComponent {
+
   private firestore: Firestore = inject(Firestore);
+  private authService: AuthServiceService = inject(AuthServiceService);
   users$: Observable<User[]>;
+  friend$: Observable<Friend[]>;
+  listUsers: User[] = [];
+  userList: User[] = [];
+  myName: string = '';
+  friendList: any[] = [];
+  myUserList: User[] = [];
+  
 
   constructor(){
     const userCollection = collection(this.firestore, 'utilisateurs');
     this.users$ = collectionData(userCollection, { idField: 'id' }) as Observable<User[]>;
+
+    const friendList = collection(this.firestore, 'friendlist');
+    this.friend$ = collectionData(friendList, { idField: 'id' }) as Observable<Friend[]>;
+
+    const currentUser = this.authService.getUser();
+    if (currentUser) {
+      this.myName = currentUser;
+    }
+    this.users$.subscribe((users) => {
+      this.myUserList = users;
+    });
+    
   }
 
-  addFriend(userId:any){
-    const friendRequests = collection(this.firestore, 'friend-requests', userId);
-    addDoc(friendRequests, { User: this.users$});
-  }
+  // getUsers() {
+  //   this.users$.subscribe((users) => {
+  //     this.listUsers = users;
+  //   });
+  // }
 
+  
+  
+  addFriend(user: any){
+    const currentUser = this.authService.getUser();
+    const friendList = collection(this.firestore, 'friendlist');
+    addDoc(friendList, {
+      name: user.name,
+      friend: currentUser,
+    });
+    this.myUserList.forEach(u => {
+      this.myUserList.slice(user, 1)
+    });
+
+
+  // getNoFriendsList(user: any) {
+  //   
+    
+    // users.subscribe((users) => {
+    //   this.listUsers = users;
+    //   this.myUserList = this.friendList.filter(
+    //     (user) => user.friend !== user.name
+    //   );
+    // });
+    }
 }
