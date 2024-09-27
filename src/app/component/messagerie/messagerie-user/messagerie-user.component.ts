bd99ba07f6479e313cddbb35b9701d193ca4d17f
Maganor22 <cataclysme21293@gmail.com>
2024-09-27 10:05:03 +0200

    Modification messagerie

---
+++ src/app/component/messagerie/messagerie-user/messagerie-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagerie-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './messagerie-user.component.html',
+  styleUrl: './messagerie-user.component.css'
+})
+export class MessagerieUserComponent {
+
+}
